6acd5c7cce6242fdc0b1a0c44d0b2629fe6a8d00
MikeJames <mikejamesusa37@gmail.com>
2025-06-29 16:10:51 +0300

    feat(routing): add dinamic urls calcs

--- calculator/calc/urls.py
+++ calculator/calc/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('calcs/<slug:calc_slug>/', views.calcs),
 ]
--- calculator/calc/views.py
+++ calculator/calc/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse, HttpRequest, HttpResponseNotFound, Http404
 
 def index(request):
     return HttpResponse('Home page calc')
+
+def calcs(request, calc_slug: str):
+    return HttpResponse(f'Calculators {calc_slug} page')
