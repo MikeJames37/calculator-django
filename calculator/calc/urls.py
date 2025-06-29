8e59ee39d44c4ba2be9efbbf62fff67975770b62
MikeJames <mikejamesusa37@gmail.com>
2025-06-30 00:26:17 +0300

    feat(views) add calc/templates and readme

--- calculator/calc/urls.py
+++ calculator/calc/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('calcs/<slug:calc_slug>/', views.calcs, name='calcs'),
+    path('about/', views.about, name='about')
 ]
--- calculator/calc/views.py
+++ calculator/calc/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest, HttpResponseNotFound, Http404
 
+menu = [
+    {'title': 'About site', 'url_name': 'about'},
+    {'title': 'Calculators', 'url_name': 'calcs'},
+    {'title': 'Add calculator', 'url_name': 'add_calc'},
+    {'title': 'Feedback', 'url_name': 'feedback'},
+    {'title': 'Login', 'url_name': 'login'},
+]
+
+data = {'title': 'Main Page',
+        'menu': menu}
+
 def index(request):
-    return HttpResponse('Home page calc')
+    return render(request, 'calc/index.html', context=data)
 
 def calcs(request, calc_slug: str):
     return HttpResponse(f'Calculators {calc_slug} page')
 
 def page_not_found(request: HttpRequest, exception: Http404):
     return HttpResponseNotFound(f'Page not found: {exception}')
+
+def about(request):
+    return render(request, 'calc/about.html', context={'title': 'About site', 'menu': menu})
